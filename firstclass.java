fe9bc45079c8cca33afede2e5c894a6f54b14ff4
Gabriele Musizza <gabmusiz@gmail.com>
2019-10-22 22:57:52 +0200

    ho accorciato l'if

--- firstclass.java
+++ firstclass.java
@@ -1,11 +1,7 @@
 public class firstclass {
 	public static void main(String[] args){
 		for (int i = 1; i<=9; i++){
-			if(i==1)
-				System.out.println(i + " pompiere + 1 pompiere = 2 pompieri");
-			else
-				System.out.println(i + " pompieri + 1 pompiere = " + (1 + i) + " pompieri");
-			 
+			System.out.println(i + (i==1?" pompiere":" pompieri") + " + 1 pompiere = " + (1 + i) + " pompieri");
 		}
 		//print("hello world!");
 	}
