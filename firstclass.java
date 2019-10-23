f2f96cf222fe56a8e36d49928c3fc4cf570d849d
Nicolò Termine <nicotermins@gmail.com>
2019-10-23 22:47:51 +0200

    fixato testo topi

--- firstclass.java
+++ firstclass.java
@@ -5,14 +5,17 @@ public class firstclass {
 		  do{
       System.out.println("1\tfilastrocca completa ");
       System.out.println("2\tnumero di pompieri");
+	  System.out.println("3\tnumero di topi");
       System.out.println("Cosa vuoi fare? -> ");
       selection = Integer.parseInt(System.console().readLine());  // Read user input
       if(selection == 1){
         filastrocca();
       }else if(selection == 2){
         inputIntFilastrocca();
-      }
-    }while(selection != 0  selection>2  selection <0);
+      }else if(selection == 3){
+		  filastroccaTopi();
+	  }
+    }while(selection != 0  selection>3  selection <0);
 
 
 
@@ -23,7 +26,7 @@ public class firstclass {
 			System.out.println(i + (i==1?" pompiere":" pompieri") + " + 1 pompiere = " + (1 + i) + " pompieri");
 		}
 	}
-	  public static void filastrocca(){
+	  public static void filastroccaTopi(){
         for (int i = 1; i<=9; i++){
             System.out.println(i + (i==1?" topo":" topi") + " + 1 topo = " + (1 + i) + " pifferaio");
         }
